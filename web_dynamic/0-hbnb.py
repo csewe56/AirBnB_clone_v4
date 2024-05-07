1b3e413f98205b3373521ce1b6c6610264997df5
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-05-07 12:35:52 -0700

    1. Cash only

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Scripts that starts a flash web application"""
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """function that removes current SQLAlchemy session"""
+    storage.close()
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """function that brings HBNB alive"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+        amenities = storage.all(Amenity).values()
+        amenities = sorted(amenities, key=lambda k: k.name)
+
+        places = storage.all(Place).values()
+        places = sorted(places, key=lambda k: k.name)
+
+    return render_template('100-hbnb.html',
+                            states=st_ct,
+                            amenities=amenities,
+                            paces=places, cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+    """Main function"""
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_dynamic/README.md
@@ -0,0 +1,6 @@
+0x06. AirBnB clone - Web dynamic
+Python
+Front-end
+JavaScript
+ Weight: 2
+ Project to be done in teams of 2 people (your team: Gloria Muthumbi, Charles Sewe)
